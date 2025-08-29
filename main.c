c843197cd028f1782262151974f64a44ed76edb4
angelodirb <168436547+angelodirb@users.noreply.github.com>
2025-08-29 10:46:23 -0500

    ejercicio 3

--- main.c
+++ main.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-int cmp_int(const void *a, const void *b)
-{ 
-    int x=*(const int*)a, y=*(const int*)b; 
-    return (x>y)-(x<y); 
-}
-int main(void){
-    int n;
-    scanf("%d",&n);
-    // TODO: malloc n, leer, qsort, compactar unicos, (opcional) realloc
-    // Salida:
-    // k
-    // a0 a1 ... a(k-1)
-    // TODO: free
+
+int main() {
+    int n = 0;
+
+    scanf("%d", &n);
+
+  
+    int *enteros = (int *)malloc(n * sizeof(int));
+    
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &enteros[i]);
+    }
+
+ 
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (enteros[i] == enteros[j]) {
+                
+                for (int k = j; k < n - 1; k++) {
+                    enteros[k] = enteros[k + 1];
+                }
+                n--;
+                j--; 
+                enteros = (int *)realloc(enteros, n * sizeof(int));
+                
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", enteros[i]);
+    }
+    printf("\n");
+
+
+    free(enteros);
+
     return 0;
 }
